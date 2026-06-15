f89428d7a8ce4f09cffaa131d83db31d56b5d25e
smishra125 <smishra125@outlook.com>
2026-06-16 01:00:07 +0530

    Second folder change cpmmit

--- cmake_gcc/build/CMakeFiles/3.30.2/CMakeSystem.cmake
+++ cmake_gcc/build/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_gateway/cmake_gcc/toolchain.cmake")
+include("C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_server/cmake_gcc/toolchain.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
--- cmake_gcc/build/cmake_install.cmake
+++ cmake_gcc/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_gateway/cmake_gcc
+# Install script for directory: C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_server/cmake_gcc
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/smartpresence_gateway")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/smartpresence_server")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_gateway/cmake_gcc/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/smish/SimplicityStudio/v6_workspace/smartpresence_server/cmake_gcc/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
